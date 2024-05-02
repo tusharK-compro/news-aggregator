d3dae9c7028fd1332b193df7d10455261190bb61
Compro-Rishabh <rishabh.singhal@comprotechnologies.com>
2024-05-02 11:35:59 +0530

    removed mui styles

--- src/components/NewsComponent.tsx
+++ src/components/NewsComponent.tsx
@@ -1,5 +1,5 @@
 import { Paper, Typography } from '@mui/material';
-import { createStyles, makeStyles } from '@mui/styles';
+
 
 import React from 'react';
 interface NewsComponentProps {
@@ -7,44 +7,12 @@ interface NewsComponentProps {
     content: string;
   }
   
-  const useStyles = makeStyles(() =>
-    createStyles({
-        root: {
-          padding: '16px',
-          marginBottom: '16px',
-          boxShadow: '0 4px 8px rgba(0,0,0,0.3)',
-          borderRadius: 8,
-          maxWidth:300,
-          backgroundColor: '#fff',
-          transition: 'transform 0.3s ease-in-out',
-          '&:hover': {
-            transform: 'translateY(-5px)',
-          },
-        },
-        title: {
-          fontSize: 18,
-          fontWeight: 'bold',
-          marginBottom: '8px',
-        },
-        content: {
-          fontSize: 16,
-        },
-      })
-  );
   
   const NewsComponent = (props:NewsComponentProps) => {
-    const classes = useStyles();
     const {title,content}=props;
   
     return (
-        <Paper className={classes.root}>
-        <Typography className={classes.title} variant="h6" component="h2">
-          {title}
-        </Typography>
-        <Typography className={classes.content} color="textSecondary" component="p">
-          {content}
-        </Typography>
-      </Paper>
+        <div></div>
   
     );
   };
