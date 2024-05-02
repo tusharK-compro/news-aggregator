8b0a56aca33d03eb6ce51adb9e17f78f56d8478c
tusharK-compro <tushar.kumar@comprotechnologies.com>
2024-05-02 11:09:18 +0530

    add mui styles

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import { Button } from '@mui/material';
+import NewsComponent from './components/NewsComponent';
 
 function Home() {
     return (
@@ -10,6 +11,8 @@ function Home() {
                 News Feed
             </h1>    
             <Button variant='contained'>Back to feed</Button>
+
+            <NewsComponent title='Goldy Brar Shot Dead' content='Oh jatt da muqabala Dass mainu kithe ae ni Jatt da muqabala Dass mainu kithe ae ni Jatt da muqabala Dass mainu kithe'/>
                 
         </div>
     );
