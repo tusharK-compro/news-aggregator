fae2f28b55b3bb5ea5ec8409c29c67194e8d37ff
Compro-Rishabh <rishabh.singhal@comprotechnologies.com>
2024-05-07 14:30:12 +0530

    added search function in services

--- src/services/NewsService.ts
+++ src/services/NewsService.ts
@@ -1,10 +1,17 @@
 import { data, options } from "./../types";
-const NYSearch = async (searchQuery: string, loadMore:boolean) => {
-  let finalData: Array<data> = [];
+const API_KEY_NA = "ebbbdff75baf47398fff8cda679b2338";
+const API_KEY_NY = "58BLGdQmfqvBkcnJhlNRI23znYnLIl6y";
+const API_KEY_TG = "f368d3ab-5350-40ae-9ecb-16ee8c8cdaf9";
 
-  const API_KEY = "58BLGdQmfqvBkcnJhlNRI23znYnLIl6y";
+export const NewsSearch = async (searchQuery:string, options: options, loadMore: boolean,
+  page:number
+) => {
+  const category = options?.category?.join();
+  const date = options?.date;
+  const sources = options?.sources?.join();
+  let finalData: Array<data> = [];
   await fetch(
-    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&api-key=${API_KEY}`
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&api-key=${API_KEY_NY}`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -20,16 +27,10 @@ const NYSearch = async (searchQuery: string, loadMore:boolean) => {
       });
     })
     .catch((error) => console.error("Error:", error));
-  return finalData;
-};
-
-export const NewsAPISearch = async () => {
-  const API_KEY = "ebbbdff75baf47398fff8cda679b2338";
-  let finalData: Array<data> = [];
-
-  await fetch(`https://newsapi.org/v2/everything?q=apple&apiKey=` + API_KEY)
-    .then((response) => response.json())
-    .then((data) => {
+  
+  await fetch(`https://newsapi.org/v2/everything?q=apple&apiKey=` + API_KEY_NA)
+  .then((response) => response.json())
+  .then((data) => {
       data.articles.forEach((data: any, index: number) => {
         if (data.title != "[Removed]") {
           let value = {
@@ -45,95 +46,105 @@ export const NewsAPISearch = async () => {
       });
     })
     .catch((error) => console.error("Error:", error));
+    fetch(`https://content.guardianapis.com/search?` + (searchQuery ? "q=" + searchQuery : "") + (category ? "&sectionName=" + category : "") + (date ? "&from-date=" + date : "") +(loadMore ? "&page=" + page : "")+"&page-size=10" +"&api-key=" + API_KEY_TG)
+        .then(response => response.json())
+        .then(data => {
+            data.response.results.forEach((data: any, index: number) => {
+                let value = {
+                    id: data.id,
+                    category: data.sectionName,
+                    date: data.webPublicationDate,
+                    title: data.webTitle,
+                    webUrl: data.webUrl
+                }
+                finalData.push(value);
+
+            })
+        })
+        .catch(error => console.error('Error:', error));
   return finalData;
 };
-export const NewsServiceEverything = async(options?: options, loadMore?:boolean, page?: number) => {
+export const NewsServiceEverything = async (options?: options, loadMore?: boolean, page?: number) => {
   let finalData: Array<any> = [];
   const category = options?.category?.join();
   const author = options?.author?.join();
   const sources = options?.sources?.join();
 
-  const API_KEY_NA = "ebbbdff75baf47398fff8cda679b2338";
-  const API_KEY_NY = "58BLGdQmfqvBkcnJhlNRI23znYnLIl6y";
-  const API_KEY_TG = "f368d3ab-5350-40ae-9ecb-16ee8c8cdaf9";
- 
-    await fetch(
-      `http://newsapi.org/v2/everything?`+
-      (category ? "q=" + category : "q=all") +
-      (loadMore ? "&page=" + page : "") +
-      "&pageSize=10" +
-      "&apiKey="  + API_KEY_NA
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
-        data.articles.forEach((data: any, index: number) => {
-            console.log(data)
-          if (data.title != "[Removed]") {
-            let value = {
-              id: index,
-              date: data.publishedAt,
-              title: data.title,
-              description: data.description,
-              webUrl: data.url,
-            };
 
-            finalData.push(value);
-          }
-        });
-      })
-      .catch((error) => console.error("Error:", error));
-  
-    await fetch(
-      `https://api.nytimes.com/svc/search/v2/articlesearch.json?` +
-      (category ? "q=" + category : "") +
-      (loadMore ? "&page=" + page : "") +
-      "&page-size=10" +
-        "&api-key=" +
-      API_KEY_NY
-    )
-    
-      .then((response) => response.json())
-      .then((data) => {
-        data.response.docs.forEach((data: any, index: number) => {
+  await fetch(
+    `http://newsapi.org/v2/everything?` +
+    (category ? "q=" + category : "q=all") +
+    (loadMore ? "&page=" + page : "") +
+    "&pageSize=10" +
+    "&apiKey=" + API_KEY_NA
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      data.articles.forEach((data: any, index: number) => {
+        console.log(data)
+        if (data.title != "[Removed]") {
           let value = {
             id: index,
-            category: data.news_desk,
-            date: data.published_date,
-            title: data.headline?.main,
-            description: data.abstract,
-            webUrl: data.web_url,
+            date: data.publishedAt,
+            title: data.title,
+            description: data.description,
+            webUrl: data.url,
           };
-          finalData.push(value);
-        });
-      })
-      .catch((error) => console.error("Error:", error));
 
-    await fetch(
-      `https://content.guardianapis.com/search?` +
-        (category ? "&sectionName=" + category : "") +
-        (loadMore ? "&page=" + page : "") +
-        "&page-size=10" +
-        "&api-key=" +
-        API_KEY_TG
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        data.response.results.forEach((data: any, index: number) => {
-          //console.log("fff ", data)
-          let value = {
-            id: data.id,
-            category: data.sectionName,
-            date: data.webPublicationDate,
-            title: data.webTitle,
-            webUrl: data.webUrl,
-          };
           finalData.push(value);
-        });
-      })
-      .catch((error) => console.error("Error:", error));
+        }
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+  
+  await fetch(
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?` +
+    (category ? "q=" + category : "") +
+    (loadMore ? "&page=" + page : "") +
+    "&page-size=10" +
+    "&api-key=" +
+    API_KEY_NY
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.response.docs.forEach((data: any, index: number) => {
+        let value = {
+          id: index,
+          category: data.news_desk,
+          date: data.published_date,
+          title: data.headline?.main,
+          description: data.abstract,
+          webUrl: data.web_url,
+        };
+        finalData.push(value);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
 
-    
+  await fetch(
+    `https://content.guardianapis.com/search?` +
+    (category ? "&sectionName=" + category : "") +
+    (loadMore ? "&page=" + page : "") +
+    "&page-size=10" +
+    "&api-key=" +
+    API_KEY_TG
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.response.results.forEach((data: any, index: number) => {
+        //console.log("fff ", data)
+        let value = {
+          id: data.id,
+          category: data.sectionName,
+          date: data.webPublicationDate,
+          title: data.webTitle,
+          webUrl: data.webUrl,
+        };
+        finalData.push(value);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
 
   return finalData;
 };
