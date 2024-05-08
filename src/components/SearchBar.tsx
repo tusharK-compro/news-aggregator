5bbf9df961e164ac5da8b4491d532eb7cf1cd933
Compro-Rishabh <rishabh.singhal@comprotechnologies.com>
2024-05-08 11:09:00 +0530

    removed search

--- src/components/SearchBar.tsx
+++
@@ -1,9 +0,0 @@
-import { TextField } from '@mui/material';
-
-function SearchBar() {
-    return (
-        <TextField id="outlined-basic" label="Search" variant="outlined" sx={{ bgcolor: "white", m: 2, borderRadius: '24px', }} />
-    );
-}
-
-export default SearchBar;
